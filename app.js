9678ed1e8b530fdcb4191e38d3eb0b23fe3fdd11
umit-turk <uyt_turk@hotmail.com>
2021-05-17 22:46:26 +0300

    odev9

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Text = require('./models/Text')
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/clean-blog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWEARS
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
-  res.render('/')
+app.get('/', async (req, res) => {
+   const textes = await Text.find({})
+   res.render('index', {
+     textes,
+   })
+  
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/textes', async (req, res) => {
+  //form aksiyonunda yazdığım yönlendirmeyi yakalıyorum.
+  await Text.create(req.body)
+  res.redirect('/');
 });
 
 const port = 3000;
